0f7d0a6c0e3a447976f64986b944f47218f91292
Manish Mishra <manishmishra786876@gmail.com>
2022-12-22 15:12:47 +0530

    classroom page added

---
+++ teamsClone/myteams/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import RenderingFirst from "./components/RenderingPages/RenderingFirst";
+import { Link,BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import GeneralComponent from "./components/TeamsInternalComponents/GeneralComponent"
+function App() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<RenderingFirst/>}/>
+      <Route exact path="/grp" element={<GeneralComponent/>}/>
+    </Routes>
+  );
+}
+
+export default App;
