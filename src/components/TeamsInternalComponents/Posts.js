7fbf4f3ecb817bb91474a701cda413d8f1ae33a3
Manish Mishra <manishmishra786876@gmail.com>
2022-12-22 22:18:08 +0530

    changes are done

---
+++ src/components/TeamsInternalComponents/Posts.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PostsCard from './PostsCard'
+
+export default function Posts() {
+  return (
+    <div>
+    <div><PostsCard/></div>
+    <div><PostsCard/></div>
+    <div><PostsCard/></div>
+    <div><PostsCard/></div>
+    <div><PostsCard/></div>
+    <div><PostsCard/></div>
+    <div><PostsCard/></div>
+    <div><PostsCard/></div>
+    <div><PostsCard/></div>
+    <div><PostsCard/></div>
+    </div>
+  )
+}
---
+++ src/components/TeamsInternalComponents/PostsCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import VideocamIcon from '@material-ui/icons/Videocam';
+
+export default function PostsCard() {
+  return (
+    <div class='flex'>
+        <div class='flex initial w-[64px]'>hello</div>
+        <div class='grid grid-cols-1'>
+            <div>First row</div>
+            <div>Second row</div>
+        </div>
+    </div>
+  )
+}
